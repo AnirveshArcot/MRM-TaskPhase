c3b35b76ae1e8da3ec5e3854f185de1dc45c1a9b
AnirveshArcot <arcotanirvesh@gmail.com>
2024-04-01 20:10:48 +0530

    gand

--- GANs/inference.py
+++ GANs/inference.py
@@ -22,14 +22,23 @@ def inference(generator, test_dataset, device):
             mse = F.mse_loss(hr_imgs, gen_hr_imgs)
             psnr = 20 * torch.log10(1.0 / torch.sqrt(mse))
             psnr_values.append(psnr.item())
+            
+            # Display ground truth high-res image
+            hr_image_ground_truth = hr_imgs[0].squeeze().permute(1, 2, 0)
+            hr_image_ground_truth = (hr_image_ground_truth - hr_image_ground_truth.min()) / (hr_image_ground_truth.max() - hr_image_ground_truth.min())
+            
             plt.figure()
-            plt.subplot(1, 2, 1)
+            plt.subplot(1, 3, 1)
             plt.imshow(lr_image_test)
             plt.title('Low Resolution')
-            plt.subplot(1, 2, 2)
+            plt.subplot(1, 3, 2)
             plt.imshow(hr_image_test)
-            plt.title('High Resolution - PSNR: {:.2f} dB'.format(psnr))
+            plt.title('Generated High Resolution - PSNR: {:.2f} dB'.format(psnr))
+            plt.subplot(1, 3, 3)
+            plt.imshow(hr_image_ground_truth)
+            plt.title('Ground Truth High Resolution')
             plt.show()
+            
             break  # Remove this line if you want to visualize all images
     avg_psnr = np.mean(psnr_values)
     print("Average PSNR:", avg_psnr)
